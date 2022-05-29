4981b7866cb661b318b0eadc696b65da385a9c3a
latzyx <1360561224@qq.com>
2022-05-29 19:34:17 +0800

    end

--- core/models/init.go
+++ core/models/init.go
@@ -1,16 +1,17 @@
 package models
 
 import (
-	_ `github.com/go-sql-driver/mysql`
-	`log`
-	`xorm.io/xorm`
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"xorm.io/xorm"
 )
 
 var Engine = InInt()
 
 func InInt() *xorm.Engine {
-	s := "root:107827@Zyx@tcp(127.0.0." +
-		"1:3306)/cloud-disk?charset=utf8mb4&parseTime=True&loc=Local"
+	s := "root:107827@zyx@tcp(127.0.0." +
+		"1:3306)/cloud_disk?charset=utf8mb4&parseTime=True&loc=Local"
 	engine, err := xorm.NewEngine("mysql", s)
 	if err != nil {
 		log.Printf("Xrom New InInt Eroor:%v", err)
